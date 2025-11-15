8bfe4ffab11fccf9e144f666cd205ce073b5f665
Aadesh Kumar <aadesh.kumar@synlabs.io>
2025-11-15 16:04:35 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import Header from '../components/Header';
-import Hero from '../components/Hero';
 
 const Home = () => {
   return (
     <div className="min-h-screen bg-white">
       <Header />
-      <main>
-        <Hero />
-      </main>
     </div>
   );
 };
